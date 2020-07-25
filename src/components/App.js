69119311458595f582fae3d6301ad2a19673e913
Rituraj <riturajguwahati26@gmail.com>
2020-07-26 04:35:36 +0530

    fixed few issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,51 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 import './App.css';
 
 class App extends React.Component {
+    state = { videos: [], videoSelected: null };
 
-	state = {videos: [], videoSelected: null};
+    onFormSubmit = async (term) => {
+        try {
+            const res = await axios.get(
+                'https://www.googleapis.com/youtube/v3/search',
+                {
+                    params: {
+                        q: term,
+                        key: 'AIzaSyDZya8QYaq3b-NK7PDCIsVokb7QQi400WI',
+                        part: 'snippet',
+                        maxResults: 10,
+                    },
+                }
+            );
+            this.setState({
+                videos: res.data.items,
+                videoSelected: res.data.items[0],
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-	onFormSubmit = async term => {
-		const res = await youtube.get('/search', {
-			params: {
-				q: term
-			}
-		});
-		this.setState({
-			videos: res.data.items,
-			videoSelected: res.data.items[0]
-		});
-	}
+    onVideoSelect = (video) => {
+        this.setState({ videoSelected: video });
+    };
 
-	onVideoSelect = video => {
-		this.setState({videoSelected: video});
-	}
-
-	render() {
-
-		return (
-			<div className="container">
-				<SearchBar onFormSubmit={this.onFormSubmit} />
-				<VideoDetail video={this.state.videoSelected} />
-				<VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
-			</div>
-		);
-	}
-
+    render() {
+        return (
+            <div className="container">
+                <SearchBar onFormSubmit={this.onFormSubmit} />
+                <VideoDetail video={this.state.videoSelected} />
+                <VideoList
+                    videos={this.state.videos}
+                    onVideoSelect={this.onVideoSelect}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
